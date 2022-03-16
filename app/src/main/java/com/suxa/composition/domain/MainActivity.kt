9dfaa011dd0e5959508da0c3ed8752a6ea38b717
suxa56 <suxa5637@mail.ru>
2022-03-16 10:28:49 +0500

    Initial commit

---
+++ app/src/main/java/com/suxa/composition/domain/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.suxa.composition.domain
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.suxa.composition.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
