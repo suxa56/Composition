dbda38cdd26cb2699a79d17147653cb450cfb074
suxa56 <suxa5637@mail.ru>
2022-03-16 11:34:48 +0500

    Create data layout

---
+++ app/src/main/java/com/suxa/composition/data/GameRepositoryImpl.kt
@@ -0,0 +1,58 @@
+package com.suxa.composition.data
+
+import com.suxa.composition.domain.entity.GameSettings
+import com.suxa.composition.domain.entity.Level
+import com.suxa.composition.domain.entity.Question
+import com.suxa.composition.domain.repository.Repository
+import kotlin.math.max
+import kotlin.math.min
+import kotlin.random.Random
+
+object GameRepositoryImpl : Repository {
+
+    private const val MIN_SUM_VALUE = 2
+    private const val MIN_ANSWER_VALUE = 1
+
+    override fun generateQuestion(maxSumValue: Int, countOfOptions: Int): Question {
+        val sum = Random.nextInt(MIN_SUM_VALUE, maxSumValue + 1)
+        val visibleNumber = Random.nextInt(MIN_ANSWER_VALUE, sum)
+        val options = HashSet<Int>()
+        val rightAnswer = sum - visibleNumber
+        options.add(rightAnswer)
+        val from = max(rightAnswer-countOfOptions, MIN_ANSWER_VALUE)
+        val to = min(maxSumValue, rightAnswer+countOfOptions)
+        while (options.size < countOfOptions) {
+            options.add(Random.nextInt(from, to))
+        }
+        return Question(sum, visibleNumber, options.toList())
+    }
+
+    override fun getGameSettings(level: Level): GameSettings {
+        return when (level) {
+            Level.TEST -> GameSettings(
+                10,
+                3,
+                50,
+                8
+            )
+            Level.EASY -> GameSettings(
+                10,
+                10,
+                70,
+                60
+            )
+            Level.MEDIUM -> GameSettings(
+                20,
+                20,
+                80,
+                40
+            )
+            Level.HARD -> GameSettings(
+                30,
+                30,
+                90,
+                40
+            )
+        }
+    }
+}
