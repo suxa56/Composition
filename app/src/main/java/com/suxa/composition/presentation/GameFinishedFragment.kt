feffcab536f1d06ba8692123f88ade0c751ccae4
suxa56 <suxa5637@mail.ru>
2022-04-05 09:12:00 +0500

    Game view model pro

--- app/src/main/java/com/suxa/composition/presentation/GameFinishedFragment.kt
+++ app/src/main/java/com/suxa/composition/presentation/GameFinishedFragment.kt
@@ -5,7 +5,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import com.suxa.composition.R
@@ -17,7 +16,8 @@ class GameFinishedFragment : Fragment() {
     private lateinit var gameResult: GameResult
 
     private var _binding: FragmentGameFinishedBinding? = null
-    private val binding get() = _binding!!
+    private val binding: FragmentGameFinishedBinding
+        get() = _binding ?: throw RuntimeException("FragmentGameFinishedBinding == null")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,10 +34,57 @@ class GameFinishedFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        putResult()
-        backPressedCallback()
+        setupClickListeners()
+        bindViews()
+    }
+
+    private fun setupClickListeners() {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                retryGame()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
         binding.buttonRetry.setOnClickListener {
             retryGame()
+        }
+    }
+
+    private fun bindViews() {
+        with(binding) {
+            emojiResult.setImageResource(getSmileResId())
+            tvRequiredAnswers.text = String.format(
+                getString(R.string.required_score),
+                gameResult.gameSettings.minCountOfRightAnswers
+            )
+            tvScoreAnswers.text = String.format(
+                getString(R.string.score_answers),
+                gameResult.countOfRightAnswers
+            )
+            tvRequiredPercentage.text = String.format(
+                getString(R.string.required_percentage),
+                gameResult.gameSettings.minPercentOfRightAnswers
+            )
+            tvScorePercentage.text = String.format(
+                getString(R.string.score_percentage),
+                getPercentOfRightAnswers()
+            )
+        }
+    }
+
+    private fun getSmileResId(): Int {
+        return if (gameResult.winner) {
+            R.drawable.ic_smile
+        } else {
+            R.drawable.ic_sad
+        }
+    }
+
+    private fun getPercentOfRightAnswers() = with(gameResult) {
+        if (countOfQuestions == 0) {
+            0
+        } else {
+            ((countOfRightAnswers / countOfQuestions.toDouble()) * 100).toInt()
         }
     }
 
@@ -52,52 +99,6 @@ class GameFinishedFragment : Fragment() {
         }
     }
 
-    private fun putResult() {
-        if (gameResult.winner) {
-            binding.emojiResult.setImageDrawable(
-                ContextCompat.getDrawable(
-                    requireContext(),
-                    R.drawable.ic_smile
-                )
-            )
-        } else {
-            binding.emojiResult.setImageDrawable(
-                ContextCompat.getDrawable(
-                    requireContext(),
-                    R.drawable.ic_sad
-                )
-            )
-        }
-        binding.tvRequiredAnswers.text = getString(
-            R.string.required_score,
-            gameResult.gameSettings.minCountOfRightQuestions.toString()
-        )
-        binding.tvScoreAnswers.text = getString(
-            R.string.score_answers,
-            gameResult.countOfRightAnswers.toString()
-        )
-        binding.tvRequiredPercentage.text = getString(
-            R.string.required_percentage,
-            gameResult.gameSettings.minPercentOfRightQuestions.toString()
-        )
-        binding.tvScorePercentage.text = getString(
-            R.string.score_percentage, (
-                    gameResult.countOfRightAnswers.toDouble()
-                            / gameResult.countOfQuestions.toDouble() * 100)
-                .toInt().toString()
-        )
-    }
-
-    private fun backPressedCallback() {
-        requireActivity().onBackPressedDispatcher.addCallback(
-            viewLifecycleOwner,
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    retryGame()
-                }
-            })
-    }
-
     private fun retryGame() {
         requireActivity().supportFragmentManager.popBackStack(
             GameFragment.NAME,
@@ -107,7 +108,7 @@ class GameFinishedFragment : Fragment() {
 
     companion object {
 
-        private const val KEY_GAME_RESULT = "GameResult"
+        private const val KEY_GAME_RESULT = "game_result"
 
         fun newInstance(gameResult: GameResult): GameFinishedFragment {
             return GameFinishedFragment().apply {
